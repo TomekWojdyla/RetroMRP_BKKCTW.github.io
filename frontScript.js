c92145ef7326e3afa760310026d67b71319711fb
TomekWojdyla <tw.kota@gmail.com>
2024-04-30 22:19:10 +0200

    all add subitems L0+3xL1+3xL2

--- frontScript.js
+++ frontScript.js
@@ -1,61 +1,17 @@
 'use strict';
-/* // OBSOLETE GAME CODE
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highscore = 0;
-
-const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
-};
-
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
-
-  //When there is no input
-  if (!guess) {
-    displayMessage('⛔ No number! ');
-
-    //When player wins the game
-  } else if (guess === secretNumber) {
-    displayMessage('🎉 Correct Number!');
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
-
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-
-    //When guess is wrong
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      displayMessage('You lost the game... 😭');
-      score--;
-      document.querySelector('.score').textContent = score;
-    }
-  }
-});
-*/
 
 //MRP BUTTONS SCRIPT
 
 //display section of product data entry
 document.querySelector('#add-product-btn').addEventListener('click', function () {
   document.querySelector('.product-details').style.visibility = 'visible';
+  document.querySelector('#add-product-btn').style.visibility = 'hidden';
   document.querySelector('#save-product-btn').style.visibility = 'visible';
 });
 
-//add subitem L1
-document.querySelector('#add-L1').addEventListener('click', function () {
-  let newItem = document.getElementById('L1');
-  // document.querySelector('#L1').style.visibility = 'visible';
-  // newItem.innerHTML = `<p>Print this text</p>`;
+//add subitem L1_1
+document.querySelector('#add-L1_1').addEventListener('click', function () {
+  let newItem = document.getElementById('L1_1');
   newItem.innerHTML = `<h1>L1</h1>
   <p>
     Product name <span><input class="product_L0" /></span>
@@ -66,5 +22,199 @@ document.querySelector('#add-L1').addEventListener('click', function () {
   <p>
     Production time<span><input type="number" class="product_L0" /></span>
   </p>
-  <button class="btn" id="add-L1">Add sub-item (L2)</button>`;
+  <button class="btn" onclick ="L2_1_1()" id="add-L2_1_1">Add sub-item 1</button>
+  <button class="btn" onclick ="L2_1_2()" id="add-L2_1_2">Add sub-item 2</button>
+  <button class="btn" onclick ="L2_1_3()" id="add-L2_1_3">Add sub-item 3</button>`;
+  document.querySelector('#add-L1_1').style.visibility = 'hidden';
 });
+
+//add subitem L2_1_1
+function L2_1_1() {
+  let newItem = document.getElementById('L2_1_1');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_1_1').style.visibility = 'hidden';
+}
+
+//add subitem L2_1_2
+function L2_1_2() {
+  let newItem = document.getElementById('L2_1_2');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_1_2').style.visibility = 'hidden';
+}
+
+//add subitem L2_1_3
+function L2_1_3() {
+  let newItem = document.getElementById('L2_1_3');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_1_3').style.visibility = 'hidden';
+}
+
+//add subitem L1_2
+document.querySelector('#add-L1_2').addEventListener('click', function () {
+  let newItem = document.getElementById('L1_2');
+  newItem.innerHTML = `<h1>L1</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn" onclick ="L2_2_1()" id="add-L2_2_1">Add sub-item 1</button>
+  <button class="btn" onclick ="L2_2_2()" id="add-L2_2_2">Add sub-item 2</button>
+  <button class="btn" onclick ="L2_2_3()" id="add-L2_2_3">Add sub-item 3</button>`;
+  document.querySelector('#add-L1_2').style.visibility = 'hidden';
+});
+
+//add subitem L2_2_1
+function L2_2_1() {
+  let newItem = document.getElementById('L2_2_1');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_2_1').style.visibility = 'hidden';
+}
+
+//add subitem L2_2_2
+function L2_2_2() {
+  let newItem = document.getElementById('L2_2_2');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_2_2').style.visibility = 'hidden';
+}
+
+//add subitem L2_2_3
+function L2_2_3() {
+  let newItem = document.getElementById('L2_2_3');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_2_3').style.visibility = 'hidden';
+}
+
+//add subitem L1_3
+document.querySelector('#add-L1_3').addEventListener('click', function () {
+  let newItem = document.getElementById('L1_3');
+  newItem.innerHTML = `<h1>L1</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn" onclick ="L2_3_1()" id="add-L2_3_1">Add sub-item 1</button>
+  <button class="btn" onclick ="L2_3_2()" id="add-L2_3_2">Add sub-item 2</button>
+  <button class="btn" onclick ="L2_3_3()" id="add-L2_3_3">Add sub-item 3</button>`;
+  document.querySelector('#add-L1_3').style.visibility = 'hidden';
+});
+
+//add subitem L2_3_1
+function L2_3_1() {
+  let newItem = document.getElementById('L2_3_1');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_3_1').style.visibility = 'hidden';
+}
+
+//add subitem L2_3_2
+function L2_3_2() {
+  let newItem = document.getElementById('L2_3_2');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_3_2').style.visibility = 'hidden';
+}
+
+//add subitem L2_3_3
+function L2_3_3() {
+  let newItem = document.getElementById('L2_3_3');
+  newItem.innerHTML = `<h1>L2</h1>
+  <p>
+    Product name <span><input class="product_L0" /></span>
+  </p>
+  <p>
+    Quantity <span><input type="number" class="product_L0" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" /></span>
+  </p>
+  <button class="btn">No subitems allowed</button>`;
+  document.querySelector('#add-L2_3_3').style.visibility = 'hidden';
+}
