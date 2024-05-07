27ea06aed9f758266981d19117db26a0521c1b7b
TomekWojdyla <tw.kota@gmail.com>
2024-05-07 16:19:59 +0200

    changes in methods for product definition

--- frontScript.js
+++ frontScript.js
@@ -7,226 +7,92 @@ document.querySelector('.again').addEventListener('click', function () {
   location.reload();
 });
 
-//display section of product data entry
+
+//Add Product - alternative route (create elements)
 document.querySelector('#add-product-btn').addEventListener('click', function () {
-  document.querySelector('.product-details').style.visibility = 'visible';
   document.querySelector('#add-product-btn').style.visibility = 'hidden';
   document.querySelector('#save-product-btn').style.visibility = 'visible';
+  document.querySelector("#product-input").innerHTML = 
+  `<div class = "input-section-content-L0">
+  <h1>L0:</h1>
+  <p>
+    Product name <span><input class="product_L0" id="L0-name" /></span>
+  </p>
+  <p>
+    Production time<span><input type="number" class="product_L0" id="L0-production-time" /></span>
+  </p>
+  <button class="btn" id="add-L1" onclick = "addSubitemL1()">Add sub-item (L1) - Max (3)</button>
+  </div>
+  <div id = "L1-input"></div>`
 });
 
-//add subitem L1_1
-document.querySelector('#add-L1_1').addEventListener('click', function () {
-  let newItem = document.getElementById('L1_1');
-  newItem.innerHTML = `<h1>L1</h1>
-  <p>
-    Product name <span><input class="product_L0" id="L1_1-name"/></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" id="L1_1-quantity"/></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" id="L1_1-production-time"/></span>
-  </p>
-  <button class="btn" onclick ="L2_1_1()" id="add-L2_1_1">Add sub-item 1</button>
-  <button class="btn" onclick ="L2_1_2()" id="add-L2_1_2">Add sub-item 2</button>
-  <button class="btn" onclick ="L2_1_3()" id="add-L2_1_3">Add sub-item 3</button>`;
-  document.querySelector('#add-L1_1').style.visibility = 'hidden';
-});
+let subitmesL1Count = 0;
 
-//add subitem L2_1_1
-function L2_1_1() {
-  let newItem = document.getElementById('L2_1_1');
-  newItem.innerHTML = `<h1>L2</h1>
+//Add subitems L1
+function addSubitemL1() {
+  if (subitmesL1Count<3) {
+  subitmesL1Count += 1;
+  const currentSubitemStructure = document.querySelector("#L1-input").outerHTML
+  const newSubitemStructure = 
+  `<div class = "input-section-content-L1" id = "L1_${subitmesL1Count}-input">
+  <h1>L1:${subitmesL1Count}</h1>
   <p>
-    Product name <span><input class="product_L0" /></span>
+    Product name <span><input class="product_L0" id="L1_${subitmesL1Count}-name" /></span>
   </p>
   <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
+    Quantity <span><input type="number" class="product_L0" id="L1_${subitmesL1Count}-quantity" /></span>
   </p>
   <p>
-    Production time<span><input type="number" class="product_L0" /></span>
+    Production time<span><input type="number" class="product_L0" id="L1_${subitmesL1Count}-production-time" /></span>
   </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_1_1').style.visibility = 'hidden';
+  <button class="btn" id="add-L2-${subitmesL1Count}" onclick = "addSubitemL2(${subitmesL1Count})">Add subitem (L2) - Max (3)</button>
+  </div>
+  <div id = "L${subitmesL1Count}-2-input"></div>`;
+  const fullSubitemStructure = currentSubitemStructure + newSubitemStructure
+  document.querySelector("#L1-input").innerHTML = fullSubitemStructure
+  };
+  if (subitmesL1Count===3) {
+    document.querySelector("#add-L1").innerHTML = `MAX subitems added!`
+  };
+  console.log(subitmesL1Count)
+};
+
+let subitemsL2Count = {
+  1:0,
+  2:0,
+  3:0
 }
 
-//add subitem L2_1_2
-function L2_1_2() {
-  let newItem = document.getElementById('L2_1_2');
-  newItem.innerHTML = `<h1>L2</h1>
+//Add subitems L2
+function addSubitemL2(L1ItemNumber) {
+  if (subitemsL2Count[L1ItemNumber]<3) {
+  subitemsL2Count[L1ItemNumber] += 1;
+  const currentSubitemStructure = document.querySelector(`#L${L1ItemNumber}-2-input`).outerHTML
+  const newSubitemStructure = 
+  `<div class = "input-section-content-L2" id = "L2_${L1ItemNumber}_${subitemsL2Count[L1ItemNumber]}-input">
+  <h1>L2:${L1ItemNumber}.${subitemsL2Count[L1ItemNumber]}</h1>
   <p>
-    Product name <span><input class="product_L0" /></span>
+    Product name <span><input class="product_L0" id="L2_${L1ItemNumber}_${subitemsL2Count[L1ItemNumber]}-name" /></span>
   </p>
   <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
+    Quantity <span><input type="number" class="product_L0" id="L2_${L1ItemNumber}_${subitemsL2Count[L1ItemNumber]}-quantity" /></span>
   </p>
   <p>
-    Production time<span><input type="number" class="product_L0" /></span>
+    Production time<span><input type="number" class="product_L0" id="L2_${L1ItemNumber}_${subitemsL2Count[L1ItemNumber]}-production-time" /></span>
   </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_1_2').style.visibility = 'hidden';
-}
+  </div>`;
+  const fullSubitemStructure = currentSubitemStructure + newSubitemStructure
+  document.querySelector(`#L${L1ItemNumber}-2-input`).innerHTML = fullSubitemStructure
+  };
+  if (subitemsL2Count[L1ItemNumber]===3) {
+    document.querySelector(`#add-L2-${L1ItemNumber}`).innerHTML = `MAX subitems added!`
+  };
+};
 
-//add subitem L2_1_3
-function L2_1_3() {
-  let newItem = document.getElementById('L2_1_3');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_1_3').style.visibility = 'hidden';
-}
-
-//add subitem L1_2
-document.querySelector('#add-L1_2').addEventListener('click', function () {
-  let newItem = document.getElementById('L1_2');
-  newItem.innerHTML = `<h1>L1</h1>
-  <p>
-    Product name <span><input class="product_L0" id="L1_2-name"/></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" id="L1_2-quantity"/></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" id="L1_2-production-time"/></span>
-  </p>
-  <button class="btn" onclick ="L2_2_1()" id="add-L2_2_1">Add sub-item 1</button>
-  <button class="btn" onclick ="L2_2_2()" id="add-L2_2_2">Add sub-item 2</button>
-  <button class="btn" onclick ="L2_2_3()" id="add-L2_2_3">Add sub-item 3</button>`;
-  document.querySelector('#add-L1_2').style.visibility = 'hidden';
-});
-
-//add subitem L2_2_1
-function L2_2_1() {
-  let newItem = document.getElementById('L2_2_1');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_2_1').style.visibility = 'hidden';
-}
-
-//add subitem L2_2_2
-function L2_2_2() {
-  let newItem = document.getElementById('L2_2_2');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_2_2').style.visibility = 'hidden';
-}
-
-//add subitem L2_2_3
-function L2_2_3() {
-  let newItem = document.getElementById('L2_2_3');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_2_3').style.visibility = 'hidden';
-}
-
-//add subitem L1_3
-document.querySelector('#add-L1_3').addEventListener('click', function () {
-  let newItem = document.getElementById('L1_3');
-  newItem.innerHTML = `<h1>L1</h1>
-  <p>
-    Product name <span><input class="product_L0" id="L1_3-name"/></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" id="L1_3-quantity"/></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" id="L1_3-production-time"/></span>
-  </p>
-  <button class="btn" onclick ="L2_3_1()" id="add-L2_3_1">Add sub-item 1</button>
-  <button class="btn" onclick ="L2_3_2()" id="add-L2_3_2">Add sub-item 2</button>
-  <button class="btn" onclick ="L2_3_3()" id="add-L2_3_3">Add sub-item 3</button>`;
-  document.querySelector('#add-L1_3').style.visibility = 'hidden';
-});
-
-//add subitem L2_3_1
-function L2_3_1() {
-  let newItem = document.getElementById('L2_3_1');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_3_1').style.visibility = 'hidden';
-}
-
-//add subitem L2_3_2
-function L2_3_2() {
-  let newItem = document.getElementById('L2_3_2');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_3_2').style.visibility = 'hidden';
-}
-
-//add subitem L2_3_3
-function L2_3_3() {
-  let newItem = document.getElementById('L2_3_3');
-  newItem.innerHTML = `<h1>L2</h1>
-  <p>
-    Product name <span><input class="product_L0" /></span>
-  </p>
-  <p>
-    Quantity <span><input type="number" class="product_L0" /></span>
-  </p>
-  <p>
-    Production time<span><input type="number" class="product_L0" /></span>
-  </p>
-  <button class="btn">No subitems allowed</button>`;
-  document.querySelector('#add-L2_3_3').style.visibility = 'hidden';
-}
+let product_json = {};
 
 //save product and display its structure
 document.querySelector('#save-product-btn').addEventListener('click', function () {
-  let product_json = {};
   product_json['productName'] = document.querySelector('#L0-name').value;
   product_json['productProductionTime'] = Number(document.querySelector('#L0-production-time').value);
 
@@ -234,46 +100,73 @@ document.querySelector('#save-product-btn').addEventListener('click', function (
   let productStructureText = '';
   let L0 = `<p>Product saved succesfully!</p>
   <p>L0 Product Name: ${product_json['productName']} </p>
-  <p>L0 Product's Production/Assembly Time: ${product_json['productProductionTime']} days </p>`;
+  <p>L0 Product's Production/Assembly Time: ${product_json['productProductionTime']} day(s) </p>`;
 
   let subItemsL1 = ``;
-  try {
-    for (let i = 1; i < 4; i++) {
+
+    for (let i = 1; i < subitmesL1Count+1; i++) {
       let nextIDName = `#L1_${i}-name`;
+      let nextSubitemName = document.querySelector(nextIDName).value;
       let nextIDQuantity = `#L1_${i}-quantity`;
+      let nextSubitemQuantity = document.querySelector(nextIDQuantity).value;
       let nextIDProductionTime = `#L1_${i}-production-time`;
-      let newSubitem = `<p>L1 Subitem ${i}</p> 
-      <li>     Product Name: ${document.querySelector(nextIDName).value}</li> 
-      <li>     Quantity: ${document.querySelector(nextIDQuantity).value}</li>
-      <li>     Production Time: ${document.querySelector(nextIDProductionTime).value} days</li>`;
+      let nextSubitemProductionTime = document.querySelector(nextIDProductionTime).value;
+      let newSubitem = `<p>L1: Subitem ${i}</p> 
+      <li>     Product Name: ${nextSubitemName}</li> 
+      <li>     Quantity: ${nextSubitemQuantity}</li>
+      <li>     Production Time: ${nextSubitemProductionTime} day(s)</li>`;
       subItemsL1 += newSubitem;
 
       //Extending Product JSON with subitems
-      let jsonKey = `SubitemL1_${i}`;
-      let jsonValue = {};
-      product_json[jsonKey] = {};
-      jsonValue['L1SubitemName'] = document.querySelector(nextIDName).value;
-      jsonValue['L1SubitemQuantity'] = Number(document.querySelector(nextIDQuantity).value);
-      jsonValue['L1SubitemProductionTime'] = Number(document.querySelector(nextIDProductionTime).value);
-      product_json[jsonKey] = jsonValue;
-    }
-    productStructure.innerHTML = productStructureText + L0 + subItemsL1;
+      let jsonKeyL1 = `SubitemL1_${i}`;
+      let jsonValueL1 = {};
+      product_json[jsonKeyL1] = {};
+      jsonValueL1['L1SubitemName'] = nextSubitemName;
+      jsonValueL1['L1SubitemQuantity'] = Number(nextSubitemQuantity);
+      jsonValueL1['L1SubitemProductionTime'] = Number(nextSubitemProductionTime);
+      // jsonValueL1['SubitemL2_1_1']={}
+      product_json[jsonKeyL1] = jsonValueL1;
 
-    document.querySelector('#L1_1').innerHTML = ``;
-    document.querySelector('#L1_2').innerHTML = ``;
-    document.querySelector('#L1_3').innerHTML = ``;
-  } catch {
-    subItemsL1 = `<p>No Subitems</p>`;
-    productStructure.innerHTML = productStructureText + L0 + subItemsL1;
-  }
+      //Listing L2 subitems
+      let subItemsL2 = ``;
+      for (let j = 1; j < subitemsL2Count[i]+1; j++) {
+        let nextL2IDName = `#L2_${i}_${j}-name`;
+        let nextL2SubitemName = document.querySelector(nextL2IDName).value;
+        let nextL2IDQuantity = `#L2_${i}_${j}-quantity`;
+        let nextL2SubitemQuantity = document.querySelector(nextL2IDQuantity).value;
+        let nextL2IDProductionTime = `#L2_${i}_${j}-production-time`;
+        let nextL2SubitemProductionTime = document.querySelector(nextL2IDProductionTime).value;
+        let newL2Subitem = `<p>L2: Subitem ${i}.${j}</p> 
+        <li>     Product Name: ${nextL2SubitemName}</li> 
+        <li>     Quantity: ${nextL2SubitemQuantity}</li>
+        <li>     Production Time: ${nextL2SubitemProductionTime} day(s)</li>`;
+        subItemsL2 += newL2Subitem;
+
+        //Extending Product JSON with subitems L2 for (i) Subitem L1
+        let jsonKeyL2 = `SubitemL2_${i}_${j}`;
+        let jsonValueL2 = {};
+        jsonValueL2['L2SubitemName'] = nextL2SubitemName;
+        jsonValueL2['L2SubitemQuantity'] = Number(nextL2SubitemQuantity);
+        jsonValueL2['L2SubitemProductionTime'] = Number(nextL2SubitemProductionTime);
+        // console.log(jsonValueL2)
+        product_json[jsonKeyL2] = jsonValueL2;
+      };
+      subItemsL1 += subItemsL2
+
+    };
+  
+  productStructure.innerHTML = productStructureText + L0 + subItemsL1;
 
   document.querySelector('#save-product-btn').style.visibility = 'hidden';
   document.querySelector('#add-product-btn').style.visibility = 'visible';
+  document.querySelector('#add-product-btn').innerHTML = 'Edit product';
   document.querySelector('.input-section-content-L0').style.visibility = 'hidden';
+  document.querySelector("#product-input").innerHTML = ``;
   console.log(product_json);
 });
 
-// Adding order for P0 product
+
+// Adding order for L0 product
 function addOrder() {
   document.querySelector('#save-order-btn').style.visibility = 'visible';
   document.querySelector('#add-order-btn').style.visibility = 'hidden';
