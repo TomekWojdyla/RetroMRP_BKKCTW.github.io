e4ccae7fa3df109ab0611a8c0fe0c00282de7664
mistraali <bartlomiejkapusta@gmail.com>
2024-05-11 17:17:24 +0200

    mode "zero-time-given" plan generation

--- createPlanScript.js
+++ createPlanScript.js
@@ -37,54 +37,60 @@ function createPlan(){
         let orderID = 1;
         let fulfillmentTime = order['time'];
 
-        //creates order sequentially in 2 nested for loops with the same algorithm
-        //L0
-        //checks time feasibility
-        productionStartL0 = fulfillmentTime - schematic['productionTime'];
-        //saves before-after stock values
-        storageBefore = stock['L0']['quantity'];
-        quantityL0 = checkStock(stock,'L0', order['quantity']);
-        //adjust time if no need for production
-        if(storageBefore != stock['L0'] && stock['L0']['quantity']>0) {productionStartL0 = fulfillmentTime};
-        //checks for time error, on success sends data and calls algorithm on another layer
-        if (productionStartL0 < 0) {
-            jsonFulfillment = {};
-            jsonFulfillment['status'] = 'unachievable';
-        } else {
-            submitOrder(jsonFulfillment, orderID, 'L0', schematic['name'], productionStartL0, fulfillmentTime, order['quantity'], quantityL0, storageBefore, stock['L0']['quantity']);
-            //L1
-            if(quantityL0 > 0) {
-                for(i=1; i <=3; i++) {
-                    //checks if subitem exists and if there is any need for going deeper
-                    if (typeof schematic[`SubitemL1_${i}`] != "undefined" && jsonFulfillment['status'] != 'unachievable') {
-                        productionStartL1 = productionStartL0 - schematic[`SubitemL1_${i}`]['productionTime'];
-                        
-                        storageBefore = stock[`L1-${i}`]['quantity'];
-                        orderQuantity = quantityL0 * schematic[`SubitemL1_${i}`]['quantity']
-                        quantityL1 = checkStock(stock, `L1-${i}`, orderQuantity);
-                        if(stock[`L1-${i}`]['quantity'] != storageBefore && stock[`L1-${i}`]['quantity']>0) productionStartL1 = productionStartL0;
-                        
-                        if (productionStartL1 < 0) {
-                            jsonFulfillment = {};
-                            jsonFulfillment['status'] = 'unachievable';
-                        } else {
-                            submitOrder(jsonFulfillment, ++orderID, `L1-${i}`, schematic[`SubitemL1_${i}`]['name'], productionStartL1, productionStartL0, orderQuantity, quantityL1, storageBefore, stock[`L1-${i}`]['quantity']);
-                            //L2
-                            if(quantityL1 > 0) {
-                                for(j=1; j <=3; j++) {
-                                    if (typeof schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`] != "undefined" && jsonFulfillment['status'] != 'unachievable') {
-                                        productionStartL2 = productionStartL1 - schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['productionTime'];
+        //getting mode, 0 - for no time give, 1 - for realization in time
+        let mode = (order['time'] > 0 ? 1 : 0)
+
+        if (mode) {
+            //Mode 1: creates order sequentially in 2 nested for loops with the same algorithm
+            
+            //L0
+            //checks time feasibility
+            productionStartL0 = fulfillmentTime - schematic['productionTime'];
+            //saves before-after stock values
+            storageBefore = stock['L0']['quantity'];
+            quantityL0 = checkStock(stock,'L0', order['quantity']);
+            //adjust time if no need for production
+            if(storageBefore != stock['L0']['quantity'] && stock['L0']['quantity']>0) {productionStartL0 = fulfillmentTime};
+            //checks for time error, on success sends data and calls algorithm on another layer
+            if (productionStartL0 < 0) {
+                jsonFulfillment = {};
+                jsonFulfillment['status'] = 'unachievable';
+            } else {
+                submitOrder(jsonFulfillment, orderID, 'L0', schematic['name'], productionStartL0, fulfillmentTime, order['quantity'], quantityL0, storageBefore, stock['L0']['quantity']);
+                //L1
+                if(quantityL0 > 0) {
+                    for(i=1; i <=3; i++) {
+                        //checks if subitem exists and if there is any need for going deeper
+                        if (typeof schematic[`SubitemL1_${i}`] != "undefined" && jsonFulfillment['status'] != 'unachievable') {
+                            productionStartL1 = productionStartL0 - schematic[`SubitemL1_${i}`]['productionTime'];
+                            
+                            storageBefore = stock[`L1-${i}`]['quantity'];
+                            orderQuantity = quantityL0 * schematic[`SubitemL1_${i}`]['quantity']
+                            quantityL1 = checkStock(stock, `L1-${i}`, orderQuantity);
+                            if(stock[`L1-${i}`]['quantity'] != storageBefore && stock[`L1-${i}`]['quantity']>0) productionStartL1 = productionStartL0;
+                            
+                            if (productionStartL1 < 0) {
+                                jsonFulfillment = {};
+                                jsonFulfillment['status'] = 'unachievable';
+                            } else {
+                                submitOrder(jsonFulfillment, ++orderID, `L1-${i}`, schematic[`SubitemL1_${i}`]['name'], productionStartL1, productionStartL0, orderQuantity, quantityL1, storageBefore, stock[`L1-${i}`]['quantity']);
+                                //L2
+                                if(quantityL1 > 0) {
+                                    for(j=1; j <=3; j++) {
+                                        if (typeof schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`] != "undefined" && jsonFulfillment['status'] != 'unachievable') {
+                                            productionStartL2 = productionStartL1 - schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['productionTime'];
+                                            
+                                            storageBefore = stock[`L2-${i}-${j}`]['quantity'];
+                                            orderQuantity = quantityL1 * schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['quantity'];
+                                            quantityL2 = checkStock(stock, `L2-${i}-${j}`, orderQuantity);
+                                            if(stock[`L2-${i}-${j}`]['quantity'] != storageBefore && stock[`L2-${i}-${j}`]['quantity']>0) productionStartL2 = productionStartL1;
                                         
-                                        storageBefore = stock[`L2-${i}-${j}`]['quantity'];
-                                        orderQuantity = quantityL1 * schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['quantity'];
-                                        quantityL2 = checkStock(stock, `L2-${i}-${j}`, orderQuantity);
-                                        if(stock[`L2-${i}-${j}`]['quantity'] != storageBefore && stock[`L2-${i}-${j}`]['quantity']>0) productionStartL2 = productionStartL1;
-                                       
-                                        if (productionStartL2 < 0) {
-                                            jsonFulfillment = {};
-                                            jsonFulfillment['status'] = 'unachievable';
-                                        } else {
-                                            submitOrder(jsonFulfillment, ++orderID, `L2-${i}-${j}`, schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['name'], productionStartL2, productionStartL1, orderQuantity, quantityL2, storageBefore, stock[`L2-${i}-${j}`]['quantity']);
+                                            if (productionStartL2 < 0) {
+                                                jsonFulfillment = {};
+                                                jsonFulfillment['status'] = 'unachievable';
+                                            } else {
+                                                submitOrder(jsonFulfillment, ++orderID, `L2-${i}-${j}`, schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['name'], productionStartL2, productionStartL1, orderQuantity, quantityL2, storageBefore, stock[`L2-${i}-${j}`]['quantity']);
+                                            }
                                         }
                                     }
                                 }
@@ -93,7 +99,67 @@ function createPlan(){
                     }
                 }
             }
+        } else {
+            //Mode 2: creates realization plan estimate
+
+            //gets minimal time possible
+            minimalTime = (function getMinimalTime(schematic, name, level, quantityNeeded){
+                let time = (stock[`L${level}${name}`]['quantity'] > quantityNeeded ? 0 : schematic['productionTime']);
+                let i = 1;
+                let maxTime = 0;
+                let checkTime = 0;
+                if (time > 0) {
+                    for(key in schematic){
+                        if (typeof schematic[key] == "object") checkTime = getMinimalTime(schematic[key], name+`-${i++}`, level+1, quantityNeeded - stock[`L${level}${name}`]['quantity']);
+                        if (checkTime > maxTime) maxTime = checkTime;
+                    }
+                }
+                return time+maxTime;
+            })(schematic, '', 0, order['quantity']);
+            
+            //L0
+
+            productionStartL0 = minimalTime - schematic['productionTime'];
+            //saves before-after stock values
+            storageBefore = stock['L0']['quantity'];
+            quantityL0 = checkStock(stock,'L0', order['quantity']);
+            //adjust time if no need for production
+            if(storageBefore != stock['L0']['quantity'] && stock['L0']['quantity']>0) {productionStartL0 = fulfillmentTime};
+            //checks for time error, on success sends data and calls algorithm on another layer
+            submitOrder(jsonFulfillment, orderID, 'L0', schematic['name'], productionStartL0, minimalTime, order['quantity'], quantityL0, storageBefore, stock['L0']['quantity']);
+            //L1
+            if(quantityL0 > 0) {
+                for(i=1; i <=3; i++) {
+                    //checks if subitem exists and if there is any need for going deeper
+                    if (typeof schematic[`SubitemL1_${i}`] != "undefined") {
+                        productionStartL1 = productionStartL0 - schematic[`SubitemL1_${i}`]['productionTime'];
+                            
+                        storageBefore = stock[`L1-${i}`]['quantity'];
+                        orderQuantity = quantityL0 * schematic[`SubitemL1_${i}`]['quantity']
+                        quantityL1 = checkStock(stock, `L1-${i}`, orderQuantity);
+                        if(stock[`L1-${i}`]['quantity'] != storageBefore && stock[`L1-${i}`]['quantity']>0) productionStartL1 = productionStartL0;  
+                        
+                        submitOrder(jsonFulfillment, ++orderID, `L1-${i}`, schematic[`SubitemL1_${i}`]['name'], productionStartL1, productionStartL0, orderQuantity, quantityL1, storageBefore, stock[`L1-${i}`]['quantity']);
+                            //L2
+                            if(quantityL1 > 0) {
+                                for(j=1; j <=3; j++) {
+                                    if (typeof schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`] != "undefined") {
+                                        productionStartL2 = productionStartL1 - schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['productionTime'];
+                                            
+                                        storageBefore = stock[`L2-${i}-${j}`]['quantity'];
+                                        orderQuantity = quantityL1 * schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['quantity'];
+                                        quantityL2 = checkStock(stock, `L2-${i}-${j}`, orderQuantity);
+                                        if(stock[`L2-${i}-${j}`]['quantity'] != storageBefore && stock[`L2-${i}-${j}`]['quantity']>0) productionStartL2 = productionStartL1;
+                                            
+                                        submitOrder(jsonFulfillment, ++orderID, `L2-${i}-${j}`, schematic[`SubitemL1_${i}`][`SubitemL2_${i}_${j}`]['name'], productionStartL2, productionStartL1, orderQuantity, quantityL2, storageBefore, stock[`L2-${i}-${j}`]['quantity']);
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
         }
+
         if (jsonFulfillment['status'] != 'unachievable') {
             jsonFulfillment['status'] = 'in-realization';
         }
